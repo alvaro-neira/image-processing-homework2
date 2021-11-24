9e63b57a79eb9941b74cab19ecd5b92ab96997ef
Alvaro Neira <alvaroneirareyes@gmail.com>
2021-11-24 09:50:04 -0300

    morning

--- tarea2_object_detection_yolo.py
+++ tarea2_object_detection_yolo.py
@@ -1,3 +1,15 @@
+import cv2
+import glob
+import os
+
+import numpy as np
+import random
+
+from IPython.display import Image, display
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
 # -*- coding: utf-8 -*-
 """tarea2_object_detection_yolo.ipynb
 
@@ -43,10 +55,6 @@ El primer paso será convertir el formato de las anotaciones como se mencionó a
 # %cd /content/data/orand-car-with-bbs/training
 # !mkdir labels
 
-import cv2
-import glob
-import os
-
 filenames = glob.glob("./annotations/*.txt")
 
 # Iteramos sobre todas las imágenes
@@ -79,7 +87,7 @@ for filename in filenames:
 
         # Obtenemos las coordenadas normalizadas
         x_center = (xmin + (width / 2)) / img_width
-        y_center = (ymin + (height/ 2)) / img_height
+        y_center = (ymin + (height / 2)) / img_height
         width = width / img_width
         height = height / img_height
 
@@ -97,18 +105,16 @@ for filename in filenames:
             outfile.write("%s\n" % line)
 
 # Ejemplo de anotación antigua
-!cat /content/data/orand-car-with-bbs/training/annotations/0001_5179655_46066.txt
+# !cat /content/data/orand-car-with-bbs/training/annotations/0001_5179655_46066.txt
 
 # Ejemplo de la misma anotación pero con el nuevo formato
-!cat /content/data/orand-car-with-bbs/training/labels/0001_5179655_46066.txt
+# !cat /content/data/orand-car-with-bbs/training/labels/0001_5179655_46066.txt
 
 """Ahora dividiremos los datos en un conjunto de entrenamiento y uno de test, en una proporción 90%-10% respectivamente."""
 
 # Commented out IPython magic to ensure Python compatibility.
 # %cd /content/data/orand-car-with-bbs/training
 
-import numpy as np
-import random
 
 filenames = glob.glob("images/*.png")
 random.shuffle(filenames)
@@ -120,7 +126,7 @@ split_index = np.rint(n * split_ratio).astype("int")
 train_split = filenames[:split_index]
 test_split = filenames[split_index:]
 
-train_dir = "/content/data/orand-car-with-bbs/training/"
+train_dir = "content/data/orand-car-with-bbs/training/"
 
 with open("train.txt", mode='w') as outfile:
     for annotation in train_split:
@@ -148,12 +154,12 @@ Para el entrenamiento seguiremos los siguientes pasos:
 # Commented out IPython magic to ensure Python compatibility.
 # Clonamos el repositorio
 # %cd /content
-!rm -rf yolov5
-!git clone https://github.com/ultralytics/yolov5
+# !rm -rf yolov5
+# !git clone https://github.com/ultralytics/yolov5
 
 # Instalamos los requisitos
 # %cd /content/yolov5
-!pip install -r requirements.txt
+# !pip install -r requirements.txt
 
 """Ahora agregaremos el siguiente archivo de configuración en el directorio ```/content/yolov5/data```, con el nombre *digits.yaml*:
 
@@ -179,14 +185,14 @@ names: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 # Commented out IPython magic to ensure Python compatibility.
 # Entrenamos el modelo
 # %cd /content/yolov5
-!python train.py --img 640 --batch 16 --epochs 10 --data digits.yaml --weights yolov5s.pt
+# !python train.py --img 640 --batch 16 --epochs 10 --data digits.yaml --weights yolov5s.pt
 
 # Guarda resultados en drive
 # !cp -r /content/yolov5/runs/train /content/drive/MyDrive/t2-train
 
 # Restaura resultados en yolo
-!mkdir /content/yolov5/runs/
-!cp -r /content/drive/MyDrive/t2-train/train /content/yolov5/runs/
+# !mkdir /content/yolov5/runs/
+# !cp -r /content/drive/MyDrive/t2-train/train /content/yolov5/runs/
 
 """### Testing"""
 
@@ -196,134 +202,132 @@ names: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 # !cp -r /content/yolov5/runs/detect /content/drive/MyDrive/t2-detect
 
 # Restaurar datos detectados
-!cp -r  /content/drive/MyDrive/t2-detect/detect/ /content/yolov5/runs/
+# !cp -r  /content/drive/MyDrive/t2-detect/detect/ /content/yolov5/runs/
 
-from IPython.display import Image, display
-
-display(Image('/content/yolov5/runs/train/exp/confusion_matrix.png'))
-display(Image('/content/yolov5/runs/train/exp/results.png'))
-display(Image('/content/yolov5/runs/train/exp/val_batch2_labels.jpg'))
-display(Image('/content/yolov5/runs/train/exp/val_batch2_pred.jpg'))
+display(Image('content/yolov5/runs/train/exp/confusion_matrix.png'))
+display(Image('content/yolov5/runs/train/exp/results.png'))
+display(Image('content/yolov5/runs/train/exp/val_batch2_labels.jpg'))
+display(Image('content/yolov5/runs/train/exp/val_batch2_pred.jpg'))
 
 """# Cálculo de accurracy conjunto de test"""
 
-import matplotlib.pyplot as plt
-import pandas as pd
-import glob
-
 """## Pruebas"""
 
-img = cv2.imread("/content/yolov5/runs/detect/exp/handwritten_MX8900201925093637024_maEnO1gFYEWnCyVkm4IPuQ.bmp")
+img = cv2.imread("content/yolov5/runs/detect/exp/handwritten_MX8900201925093637024_maEnO1gFYEWnCyVkm4IPuQ.bmp")
 plt.imshow(img)
 
-!cat /content/yolov5/runs/detect/exp/labels/handwritten_MX8900201925093637024_maEnO1gFYEWnCyVkm4IPuQ.txt
+# !cat /content/yolov5/runs/detect/exp/labels/handwritten_MX8900201925093637024_maEnO1gFYEWnCyVkm4IPuQ.txt
 
-#ordenando los objetos encontrados de izquierda a derecha (por ejemplo, 
-#en base a la coordenada x de la esquina superior izquierda del bounding box, o a la coordenada x del centro).
-data = pd.read_csv('/content/yolov5/runs/detect/exp/labels/handwritten_MX8900201925093637024_maEnO1gFYEWnCyVkm4IPuQ.txt', header = None, sep=" ")
+# ordenando los objetos encontrados de izquierda a derecha (por ejemplo,
+# en base a la coordenada x de la esquina superior izquierda del bounding box, o a la coordenada x del centro).
+data = pd.read_csv(
+    'content/yolov5/runs/detect/exp/labels/handwritten_MX8900201925093637024_maEnO1gFYEWnCyVkm4IPuQ.txt', header=None,
+    sep=" ")
 data = data.sort_values(by=1)
 values = data[0].values.tolist()
 int(''.join(str(val) for val in values))
 
 """## Cálculo total"""
 
-!cp /content/data/orand-car-with-bbs/test/list.txt /content/yolov5/runs/list.txt
+
+# !cp /content/data/orand-car-with-bbs/test/list.txt /content/yolov5/runs/list.txt
 
 class Metrics:
-  def __init__(self):
-    self.positives = []
-    self.negatives = []
-    self.accuraccy = 0
+    def __init__(self):
+        self.positives = []
+        self.negatives = []
+        self.accuraccy = 0
 
-  def calculate_accuracy(self):
+    def calculate_accuracy(self):
 
-      # Definición de los archivos de detección a recorrer
-      data_dir = "/content/yolov5/runs/detect/exp/labels/"
-      labels_files = glob.glob(data_dir + "*.txt")
+        # Definición de los archivos de detección a recorrer
+        data_dir = "content/yolov5/runs/detect/exp/labels/"
+        labels_files = glob.glob(data_dir + "*.txt")
 
-      # Las etiquetas reales se llevan a DataFrame
-      real = pd.read_csv('/content/yolov5/runs/list.txt', header = None, sep="\t")
+        # Las etiquetas reales se llevan a DataFrame
+        real = pd.read_csv('content/yolov5/runs/list.txt', header=None, sep="\t")
 
-      # Contadores para cálculo de métricas
-      n_positive = 0
-      n_negative = 0
-      n_total = len(labels_files)
+        # Contadores para cálculo de métricas
+        n_positive = 0
+        n_negative = 0
+        n_total = len(labels_files)
 
-      # Iteramos sobre los archivos de detección
-      for idx, label_file in enumerate(labels_files):
+        # Iteramos sobre los archivos de detección
+        for idx, label_file in enumerate(labels_files):
 
-        # Se obtiene el nombre del archivo txt y su imagen correspondiente
-        name_file = label_file.split('/')[7]
-        img_file = name_file.replace('.txt', '.bmp')
+            # Se obtiene el nombre del archivo txt y su imagen correspondiente
+            name_file = label_file.split('/')[7]
+            img_file = name_file.replace('.txt', '.bmp')
 
-        # Se obtiene el valor detectado en el archivo utilizando DataFrame
-        # ordenando los dígitos encontrados en base a la coordenada x
-        data = pd.read_csv(data_dir+name_file, header = None, sep=" ")
-        data = data.sort_values(by=1)
+            # Se obtiene el valor detectado en el archivo utilizando DataFrame
+            # ordenando los dígitos encontrados en base a la coordenada x
+            data = pd.read_csv(data_dir + name_file, header=None, sep=" ")
+            data = data.sort_values(by=1)
 
-        # Se concatenan los dígitos para obtener el valor final
-        values = data[0].values.tolist()
-        amount = int(''.join(str(val) for val in values))
+            # Se concatenan los dígitos para obtener el valor final
+            values = data[0].values.tolist()
+            amount = int(''.join(str(val) for val in values))
 
-        # Se obtiene el valor real del archivo
-        real_amount = (real.loc[real[0] == img_file])[1].values[0]
+            # Se obtiene el valor real del archivo
+            real_amount = (real.loc[real[0] == img_file])[1].values[0]
 
-        record = {
-            'file': img_file,
-            'real': real_amount,
-            'detected': amount,
-        }
-        
-        # Se compara el monto detectado con el monto real
-        if (amount == real_amount ):
-          self.positives.append(record)
-          n_positive += 1
-        else:
-          self.negatives.append(record)
-          n_negative += 1
+            record = {
+                'file': img_file,
+                'real': real_amount,
+                'detected': amount,
+            }
 
-        # Se imprime información
-        log = """
+            # Se compara el monto detectado con el monto real
+            if (amount == real_amount):
+                self.positives.append(record)
+                n_positive += 1
+            else:
+                self.negatives.append(record)
+                n_negative += 1
+
+            # Se imprime información
+            log = """
           ---- Calculating File {} ----
           detected amount: {}
           real amount:     {}
           positive:        {}
           -----------------------------
           """.format(
-              name_file,
-              amount,
-              real_amount,
-              amount == real_amount
-          )
-        print(log)
+                name_file,
+                amount,
+                real_amount,
+                amount == real_amount
+            )
+            print(log)
 
-      # Se calcula el accuraccy
-      self.accuraccy = n_positive / n_total
+        # Se calcula el accuraccy
+        self.accuraccy = n_positive / n_total
 
-      # Muestra resultados
-      result = {
-          'n_positive: ': n_positive,
-          'n_negative: ': n_negative,
-          'n_total: ': n_total,
-          'accuracy': self.accuraccy
-      }
-      print(result)
+        # Muestra resultados
+        result = {
+            'n_positive: ': n_positive,
+            'n_negative: ': n_negative,
+            'n_total: ': n_total,
+            'accuracy': self.accuraccy
+        }
+        print(result)
 
-  def show_negative_results(self):
-    data_test_dir = "/content/data/orand-car-with-bbs/test/images/"
-    fig = plt.figure(figsize=(20, 40))
-    rows = 15
-    columns = 5
-    position = 1
+    def show_negative_results(self):
+        data_test_dir = "content/data/orand-car-with-bbs/test/images/"
+        fig = plt.figure(figsize=(20, 40))
+        rows = 15
+        columns = 5
+        position = 1
 
-    for img in self.negatives:
-      image = cv2.imread(data_test_dir+img['file'])
-      title = "real: {} \n detected: {}".format(img['real'], img['detected'])
-      fig.add_subplot(rows, columns, position)
-      plt.imshow(image)
-      plt.axis('off')
-      plt.title(title)
-      position += 1
+        for img in self.negatives:
+            image = cv2.imread(data_test_dir + img['file'])
+            title = "real: {} \n detected: {}".format(img['real'], img['detected'])
+            fig.add_subplot(rows, columns, position)
+            plt.imshow(image)
+            plt.axis('off')
+            plt.title(title)
+            position += 1
+
 
 m = Metrics()
 m.calculate_accuracy()
